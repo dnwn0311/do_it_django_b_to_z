a14c5cc82228cd158bc88bad53c348b186f6689f
dnwn <dnwn378518@gmail.com>
2023-09-25 17:50:03 +0900

    포스트 상세 페이지에 부트스트랩 적용하기

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
 #
--- do_it_django_prj/wsgi.py
+++ do_it_django_prj/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for do_it_django_prj project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
 
