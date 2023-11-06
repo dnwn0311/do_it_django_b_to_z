a42633ca1e483906f5a8e087b10921665a4a903d
dnwn <dnwn378518@gmail.com>
2023-11-06 15:23:51 +0900

    카테고리 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -26,6 +31,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
@@ -37,5 +43,3 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
-
-
